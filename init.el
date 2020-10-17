eb2478fe8305d4926e50a8ffec0e1c9292d7f8f0
Null NaN <nullnan0x0000@gmail.com>
2020-10-17 23:49:07 +0800

    more package configure

--- init.el
+++ init.el
@@ -21,12 +21,38 @@
 ;; Key Binding for configure file
 (global-set-key (kbd "<f12>") 'open-init-file)
 
+;; Smex
+(require 'smex)
+(smex-initialize)
+;; (global-set-key (kbd "M-x") 'smex)
 
+;; Swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h o") 'counsel-describe-symbol)
+(global-set-key (kbd "C-h l") 'counsel-find-library)
+(global-set-key (kbd "C-h i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "C-h u") 'counsel-unicode-char)
 
+;; Smartparens
+(require 'smartparens-config)
+(smartparens-global-mode t)
 ;; Misc
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (global-company-mode 1)
 (global-hl-line-mode t)
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
 
 ;; Theme
 (load-theme 'monokai t)
@@ -52,6 +78,11 @@
 		;; solarized-theme
 		restart-emacs
 		hungry-delete
+		smex
+		swiper
+		counsel
+		ivy
+		smartparens
     		) "Default packages")
 
  (setq package-selected-packages my/packages)
@@ -79,3 +110,10 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Consolas ligaturized v2" :foundry "MS  " :slant normal :weight normal :height 120 :width normal)))))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1))
