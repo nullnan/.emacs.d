28b56f3c1d668c0d2fb6d0a831d43ca2e5885872
Null NaN <nullnan0x0000@gmail.com>
2020-11-03 23:33:40 +0800

    better company

--- lisp/code/init-company.el
+++ lisp/code/init-company.el
@@ -18,7 +18,13 @@
 		        company-begin-commands '(self-insert-command)))
 
 (use-package company-posframe
+  :diminish (company-box-mode "")
   :after company
   :init (company-posframe-mode 1))
+
+(use-package company-box
+  :diminish (company-box-mode "")
+  :after company
+  :hook (company-mode . company-box-mode))
 
 (provide 'init-company)
